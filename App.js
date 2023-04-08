e04b81fa92b44312b3bf276e3e79b3a04bef59c3
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-04-08 20:07:54 +0200

    Build the Button Component

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#002366',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
---
+++ components/AppButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+
+function Button({buttonText}) {
+  return (
+    <TouchableOpacity style={styles.container}>
+        <Text style={styles.text}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F0F8FF",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    borderRadius: 25,
+  },
+  text: {
+    color: "#000000",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontSize: 21,
+    fontWeight: 700
+  }
+});
+
+export default Button;
