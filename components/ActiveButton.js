a36a68681a8819ecd96466a426c4735c35d1369d
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-04-12 22:47:23 +0200

    Build the ActiveButton Component

---
+++ components/ActiveButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import defaultStyles from "../config/styles";
+
+function ActiveButton({text}) {
+  return (
+    <TouchableOpacity style={styles.container}>
+        <Text style={ styles.text }>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 40,
+    backgroundColor: "#FFD700",
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 21,
+    color: "#002366",
+  }
+});
+
+export default ActiveButton;
