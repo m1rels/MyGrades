b1b2f3e2a7983b89acbff18a4d1a36424d469f33
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-04-12 22:46:46 +0200

    Adjust styles.js

--- config/styles.js
+++ config/styles.js
@@ -3,6 +3,7 @@ import { Platform } from "react-native"
 export default {
     text: {
         fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-        color: "#F0F8FF"
+        color: "#F0F8FF",
+        fontWeight: "500"
     }
 }
