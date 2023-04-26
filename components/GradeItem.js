600e9a7502d5beff0a8973cb2d998d79f44de01c
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-04-26 20:50:45 +0200

    Build the GradeItem Component

---
+++ components/GradeItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import AppText from './AppText';
+import defaultStyles from "../config/styles";
+
+function GradeItem(props) {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>Schriftlich</Text>
+        <Text style={[defaultStyles.text, styles.grade]}>3</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    backgroundColor: "#002366",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    paddingHorizontal: 25,
+  },
+  text: {
+    fontSize: 18,
+    color: "#FFD700",
+    fontWeight: "700",
+  },
+  grade: {
+    fontWeight: "700",
+    fontSize: 18
+  }
+});
+
+export default GradeItem;
