d669137c72f920e7d9952ed6917867c540106ff3
Mirel Korajac <mirelkorajac@gmail.com>
2023-08-26 22:32:09 +0200

    Add collapsible functionality to SettingsItem

--- components/SettingsItem.js
+++ components/SettingsItem.js
@@ -3,15 +3,20 @@ import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import AppText from './AppText';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-function SettingsItem({ title, onPress }) {
+function SettingsItem({ title, onPress, collapsibleActive }) {
+  const noCollapsible = collapsibleActive === undefined ? true : collapsibleActive;
+
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-        <View style={styles.item}><AppText>{title}</AppText></View>
-        <MaterialCommunityIcons size={20} name="chevron-right" color="#c3cdca"/>
+      <View style={styles.item}><AppText>{title}</AppText></View>
+      {noCollapsible ? (
+        <MaterialCommunityIcons size={20} name="chevron-right" color="#c3cdca" />
+      ) : (
+        <MaterialCommunityIcons size={20} name="chevron-down" color="#c3cdca" />
+      )}
     </TouchableOpacity>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     display: "flex",
