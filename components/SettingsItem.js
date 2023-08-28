f5773cc87c74bb032abd07e8ae0eddf13496a015
Mirel Korajac <mirelkorajac@gmail.com>
2023-08-28 22:47:39 +0200

    Build WeightItem and expand SettingsItem

--- components/SettingsItem.js
+++ components/SettingsItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import AppText from './AppText';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import defaultStyles from "../config/styles";
 
-function SettingsItem({ title, onPress, collapsibleActive }) {
+function SettingsItem({ title, onPress, collapsibleActive, weight=false, weightNumber, fontSize=18, fontWeight= 500 }) {
   const noCollapsible = collapsibleActive === undefined ? true : collapsibleActive;
 
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-      <View style={styles.item}><AppText>{title}</AppText></View>
+      <View style={styles.item}><Text style={[defaultStyles.text, {fontSize: fontSize, fontWeight: fontWeight}]}  > {weight ? <Text style={{ color: '#FFD700', fontWeight: 700 }}>{weightNumber}</Text> : null}{ weight ? "x " + title : title }</Text></View>
       {noCollapsible ? (
         <MaterialCommunityIcons size={20} name="chevron-right" color="#c3cdca" />
       ) : (
@@ -26,6 +27,9 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     flexWrap: "wrap"
   },
+  weight: {
+    color: "#FFD700",
+  }
 });
 
 export default SettingsItem;
